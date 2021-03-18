c077a9207eef4e86576e9282524815fad0ef6099
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-03-18 21:52:38 +0900

    구간 합 구하기

---
+++ C++/back_11660.cpp
@@ -0,0 +1,34 @@
+#pragma warning(disable:4996)
+#include<iostream>
+#define endl "\n"
+#define MAX 1024
+
+using namespace std;
+
+int dp[MAX + 1][MAX + 1] = { 0, };
+
+
+int main(void)
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	// 입출력 최적화
+
+	int N, M;
+	int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int num = 0;
+			cin >> num;
+			dp[i + 1][j + 1] = dp[i][j + 1] + num + dp[i + 1][j] - dp[i][j];
+		}
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1] << endl;
+	}
+}
