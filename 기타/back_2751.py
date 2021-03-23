91ff1aaab9fbdbf54b2714053d089b57cdeb3dd2
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-03-24 00:24:19 +0900

    수 정렬하기

---
+++ 기타/back_2751.py
@@ -0,0 +1,16 @@
+"""
+    수 정렬하기
+"""
+
+import sys
+
+N = int(sys.stdin.readline())
+
+arr = [0] * N
+
+for i in range(N):
+    arr[i] = int(sys.stdin.readline())
+
+
+for i in sorted(arr):
+    sys.stdout.writelines("{}\n".format(i))
