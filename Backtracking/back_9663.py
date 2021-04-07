c10a0b5d1e3ed5b9c86b02a33deb834a277039bb
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-04-07 12:45:09 +0900

    N-Queen

---
+++ Backtracking/back_9663.py
@@ -0,0 +1,28 @@
+"""
+    N-Queen
+
+    공격 당하는 위치에 있는지 간단하게 판단하는 방법 생각하기
+    출처: https://rebas.kr/761 [PROJECT REBAS]
+"""
+n, ans = int(input()), 0
+a = [False]*n
+b = [False]*(2*n-1)
+c = [False]*(2*n-1)
+
+print(a)  # 세로 줄
+print(b)  # / 대각선
+print(c)  # \ 대각선
+
+def solve(i):
+    global ans
+    if i == n:
+        ans += 1
+        return
+    for j in range(n):
+        if not (a[j] or b[i+j] or c[i-j+n-1]):
+            a[j] = b[i+j] = c[i-j+n-1] = True
+            solve(i+1)
+            a[j] = b[i+j] = c[i-j+n-1] = False
+
+solve(0)
+print(ans)
