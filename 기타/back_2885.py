99662f46b2b1b6ec800588285793e830b7f35161
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-04-13 00:47:53 +0900

    알람시계

---
+++ 기타/back_2885.py
@@ -0,0 +1,17 @@
+"""
+    if 문
+    알람시계
+"""
+
+h, m = map(int, input().split())
+
+om = m - 45
+
+if om < 0:
+    oh = h - 1
+    if oh < 0:
+        print('{} {}'.format(24 + oh, 60 + om))
+    else:
+        print('{} {}'.format(oh, 60 + om))
+else:
+    print('{} {}'.format(h, om))
