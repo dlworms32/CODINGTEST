028541a4865240561720edeb4357b7a7f024f95c
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-04-30 01:26:12 +0900

    드래곤 커브

--- C++/back_15685.cpp
+++ C++/back_15685.cpp
@@ -1,7 +1,7 @@
-#pragma warning(disable:4996)
+﻿#pragma warning(disable:4996)
 #include<iostream>
 #define endl "\n"
-#define MAX 100
+#define MAX 101
 
 using namespace std;
 
@@ -15,7 +15,7 @@ int main(void)
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	// ����� ����ȭ
+	// 입출력 최적화
 
 	int n = 0;
 	cin >> n;
@@ -24,60 +24,38 @@ int main(void)
 		int x, y, d, g;
 		cin >> y >> x >> d >> g;
 		
-		int curve[1024] = { 0, };
-		int len = 1;
-		curve[0] = d;
+		int curve[1024] = { 0, };  // 최대 2^10개의 점
+		int len = 1;  // 커브의 길이
+		curve[0] = d; // 시작점에서의 방향
 
 		M[x][y] = 1;
-		for (int j = 0; j < g; j++)
+		for (int j = 0; j < g; j++) // 세대 수 만큼 이어 나감
 		{
 			for (int k = len - 1; k > -1; k--)
 			{
-				curve[len++] = (curve[k] + 1) % 4;
+				curve[len++] = (curve[k] + 1) % 4;  // 방향의 변화를 계속 추가해줌
 			}
 		}
-		//cout << "len = " << len << endl;
 		
-		for (int i = 0; i < len; i++)
+		for (int i = 0; i < len; i++)  // 방향의 변화를 맵에 반영
 		{
 			x = dx[curve[i]] + x;
 			y = dy[curve[i]] + y;
-			//cout << "next x :" << x << " next y :" << y << endl;
 			M[x][y] = 1;
-
-			//for (int k = 0; k < 100; k++) {
-			//	for (int j = 0; j < 100; j++)
-			//	{
-			//		if (M[k][j])
-			//		{
-			//			cout << "�� ";
-			//		}
-			//		else
-			//		{
-			//			cout << "�� ";
-			//		}
-
-			//	}
-			//	cout << endl;
-			//}
 		}
-
-		
-
 	}
 	
 	int count = 0;
-	for (int i = 0; i < 99; i++) {
-		for (int j = 0; j < 99; j++)
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 100; j++)
 		{
-			if (M[i][j])
+			if (M[i][j])  // 4점이 모두 1인 경우 세기
 			{
 				if (M[i + 1][j] && M[i][j + 1] && M[i + 1][j + 1])
 				{
 					count++;
 				}
 			}
-
 		}
 	}
 	cout << count << endl;;
