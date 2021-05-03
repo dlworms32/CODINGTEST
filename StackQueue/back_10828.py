4cf653dea774f8d92b1a3b311932d451ebeda514
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-05-03 15:41:29 +0900

    스택

---
+++ StackQueue/back_10828.py
@@ -0,0 +1,32 @@
+"""
+    스택
+
+"""
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+
+queue = deque()
+for _ in range(N):
+    cmd = sys.stdin.readline().split()
+
+    if cmd[0] == 'push':
+        queue.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if len(queue) > 0:
+            print(queue.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(queue))
+    elif cmd[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'top':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[len(queue) - 1])
